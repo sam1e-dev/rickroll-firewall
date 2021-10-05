82dabf036346fff5c29dd6d22f458ef333f799fe
sam1e-dev <64969067+sam1e-dev@users.noreply.github.com>
2021-10-05 20:56:30 +0200

    Create firewall.py
    First version

---
+++ firewall.py
@@ -0,0 +1,14 @@
+import os
+
+input('Press any button to start blocking (make sure this is being ran as admin please)')
+
+os.system('cls')
+os.system("echo 127.0.0.1    latlmes.com     #RickRoll Site >> C:\Windows\System32\drivers\etc\hosts")
+print('latlmes.com blocked!')
+os.system("echo 127.0.0.1    thisworldthesedays.com     #RickRoll Site >> C:\Windows\System32\drivers\etc\hosts")
+print('thisworldthesedays.com blocked!')
+os.system("echo 127.0.0.1    lasesp.com     #RickRoll Site >> C:\Windows\System32\drivers\etc\hosts")
+print('lasesp.com blocked!')
+os.system("echo 127.0.0.1    rr.noordstar.me     #RickRoll Site >> C:\Windows\System32\drivers\etc\hosts")
+print('rr.noordstar.me blocked!')
+print('To unblock these sites again please open C:\Windows\System32\drivers\etc\hosts and delete them.')
